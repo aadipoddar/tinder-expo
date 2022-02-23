fc7aed64e6d6d86efdb92d8978f4a8d898c13abb
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-23 15:17:11 +0530

    Building Home Page

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { AuthProvider } from './hooks/useAuth'
 import StackNavigator from './StackNavigator'
+import { LogBox } from 'react-native'
+LogBox.ignoreAllLogs()
 
 export default function App() {
   return (
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView, View, TouchableOpacity, Image, StatusBar, Text, StyleSheet } from 'react-native'
+import { SafeAreaView, View, TouchableOpacity, StatusBar, Text, StyleSheet, Image } from 'react-native'
 import { AntDesign, Entypo, Ionicons } from '@expo/vector-icons'
 import tw from 'tailwind-rn'
 import useAuth from '../hooks/useAuth'
 import Swiper from 'react-native-deck-swiper'
-import { useRef } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { collection, doc, onSnapshot } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const DUMMY_DATA = [
     {
@@ -37,7 +39,59 @@ const HomeScreen = () => {
 
     const navigation = useNavigation()
     const { user, logout } = useAuth()
+    const [profiles, setProfiles] = useState([])
     const swipeRef = useRef(null)
+
+    // useLayoutEffect(() =>
+    //     onSnapshot(doc(db, 'users', user.uid),
+    //         snapshot => {
+    //             if (!snapshot.exists())
+    //                 navigation.navigate('Modal')
+    //         }))
+
+    useLayoutEffect(() => {
+        navigation.setOptions({ headerShown: false })
+        onSnapshot(doc(db, 'users', user.uid), (snap) => {
+            if (!snap.exists()) {
+                navigation.navigate('Modal')
+            }
+        })
+    })
+
+    // useEffect(() => {
+    //     let unsub
+
+    //     const fetchCards = async () => {
+    //         unsub = onSnapshot(collection(db, 'users'), snapshot => {
+    //             setProfiles(
+    //                 snapshot.docs.filter(doc => doc.id !== user.uid).map(
+    //                     doc => ({
+    //                         id: doc.id,
+    //                         ...doc.data()
+    //                     })))
+    //         })
+    //     }
+
+    //     fetchCards()
+    //     return unsub
+    // })
+
+    useEffect(() => {
+        let unsub;
+        const fetchCards = async () => {
+            unsub = onSnapshot(collection(db, "users"), (snap) => {
+                setProfiles(
+                    snap.docs
+                        .filter((doc) => doc.id !== user.uid)
+                        .map((doc, id) => {
+                            return { id: doc.id, ...doc.data() };
+                        })
+                );
+            });
+        };
+        fetchCards();
+        return unsub;
+    }, []);
 
     return (
         <SafeAreaView style={[tw('flex-1'), { marginTop: StatusBar.currentHeight }]}>
@@ -64,7 +118,7 @@ const HomeScreen = () => {
                 <Swiper
                     ref={swipeRef}
                     containerStyle={{ backgroundColor: 'transparent' }}
-                    cards={DUMMY_DATA}
+                    cards={profiles}
                     stackSize={5}
                     cardIndex={0}
                     animateCardOpacity
@@ -93,7 +147,7 @@ const HomeScreen = () => {
                             }
                         }
                     }}
-                    renderCard={card => (
+                    renderCard={card => card ? (
                         <View
                             key={card.id}
                             style={tw('relative bg-white h-3/4 rounded-xl')}
@@ -106,13 +160,24 @@ const HomeScreen = () => {
                             <View style={[tw('absolute bottom-0 bg-white w-full flex-row justify-between items-center h-20 px-6 py-2 rounded-b-xl'), styles.cardShadow]}>
                                 <View>
                                     <Text style={tw('text-xl font-bold')}>
-                                        {card.firstName} {card.lastName}
+                                        {card.displayName}
                                     </Text>
                                     <Text>{card.job}</Text>
                                 </View>
 
                                 <Text style={tw('text-2xl font-bold')}>{card.age}</Text>
                             </View>
+                        </View>
+                    ) : (
+                        <View style={[tw('relative bg-white h-3/4 rounded-xl justify-center items-center'), styles.cardShadow,]}>
+                            <Text style={tw('font-bold pb-5')}>No more Profiles</Text>
+
+                            <Image
+                                style={tw('h-20 w-20')}
+                                height={100}
+                                width={100}
+                                source={{ uri: 'https://links.papareact.com/6gb' }}
+                            />
                         </View>
                     )}
                 />
