cc019031dfb94e192019449f9c3755f23c3a33f0
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-23 20:09:08 +0530

    Build the Chat List Section of the Chat Screen

---
+++ components/ChatRow.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const ChatRow = ({ matchDetails }) => {
+    return (
+        <View>
+            <Text>ChatRow</Text>
+        </View>
+    )
+}
+
+export default ChatRow
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,12 @@
 import { SafeAreaView, StatusBar } from "react-native"
+import ChatList from "../components/ChatList"
 import Header from "../components/Header"
 
 const ChatScreen = () => {
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
       <Header title='Chat' />
+      <ChatList />
     </SafeAreaView>
   )
 }
